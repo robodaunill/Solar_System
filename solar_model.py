595888afe91be8fe363b377f43385d1ef4d4da5e
robodaunill <singit_d@mail.ru>
2023-11-29 18:15:37 +0300

    исправил подсчет силы и передвиг объекта

--- solar_model.py
+++ solar_model.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # license: GPLv3
-
+import math
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
@@ -19,8 +19,10 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        F = gravitational_constant * body.m * obj.m / r**2
+        angle = math.atan2((obj.y - body.y), (obj.x - body.x))
+        body.Fx += F * math.cos(angle)
+        body.Fy += F * math.sin(angle)
 
 
 def move_space_object(body, dt):
@@ -32,9 +34,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx *dt
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    ay = body.Fy / body.m
+    body.y += body.Vy * dt
+    body.Vy += ay * dt
+
 
 
 def recalculate_space_objects_positions(space_objects, dt):
